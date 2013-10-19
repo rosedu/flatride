80269451b972647deffc68c9e7aaf46cefcdf526
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2013-10-19 18:07:33 +0300

    autocomplete

--- src-cljs/flatride/frontend.cljs
+++ src-cljs/flatride/frontend.cljs
@@ -5,16 +5,22 @@
         [dommy.core :as dommy]
         [flatride.core :as core]))
 
-(def gmaps nil)
+(def gautocomplete-from nil)
+(def gautocomplete-to nil)
 
 (defn map-config-obj []
   (js-obj "zoom" 12
-          "mapTypeId" gmaps.MapTypeId.ROADMAP
-          "center" (gmaps.LatLng. 45 45)))
+          "mapTypeId" window/google.maps.MapTypeId.ROADMAP
+          "center" (window/google.maps.LatLng. 45 45)))
+
+
+(defn init-autocomplete []
+  (set! gautocomplete-from (window/google.maps.places.Autocomplete. (sel1 :#input-from)))
+  (set! gautocomplete-to (window/google.maps.places.Autocomplete. (sel1 :#input-to))))
 
 (defn init []
-  (set! gmaps window/google.maps)
-  (gmaps.Map. (sel1 :#div-map-canvas) (map-config-obj)))
+  (init-autocomplete)
+  (window/google.maps.Map. (sel1 :#div-map-canvas) (map-config-obj)))
 
 (set! (.-onload js/window) init)
 
