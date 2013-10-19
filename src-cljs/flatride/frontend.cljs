57fa4c2664131297c33e7eae14863e595792b0e3
Andrei Oprea <andrei.br92@gmail.com>
2013-10-19 17:34:21 +0300

    Add google maps

--- src-cljs/flatride/frontend.cljs
+++ src-cljs/flatride/frontend.cljs
@@ -5,6 +5,12 @@
         [dommy.core :as dommy]
         [flatride.core :as core]))
 
-(dommy/append! (sel1 :body) (node [:p "Helloes"]))
+(defn map-config-obj []
+  (js-obj "zoom" 12
+                "mapTypeId" window/google.maps.MapTypeId.ROADMAP
+                "center" (window/google.maps.LatLng. 45 45))) 
 
+(defn init []
+  (window/google.maps.Map. (sel1 :#div-map-canvas) (map-config-obj)))
 
+(set! (.-onload js/window) init)
