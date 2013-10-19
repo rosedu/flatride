14315d26c2733168beb2bbf1ffc99adb28994ef2
Mihnea Dobrescu-Balaur <mihnea@linux.com>
2013-10-19 17:45:31 +0300

    alias gmaps

--- src-cljs/flatride/frontend.cljs
+++ src-cljs/flatride/frontend.cljs
@@ -5,12 +5,16 @@
         [dommy.core :as dommy]
         [flatride.core :as core]))
 
+(def gmaps nil)
+
 (defn map-config-obj []
   (js-obj "zoom" 12
-                "mapTypeId" window/google.maps.MapTypeId.ROADMAP
-                "center" (window/google.maps.LatLng. 45 45)))
+                "mapTypeId" gmaps.MapTypeId.ROADMAP
+                "center" (gmaps.LatLng. 45 45)))
 
 (defn init []
-  (window/google.maps.Map. (sel1 :#div-map-canvas) (map-config-obj)))
+  (set! gmaps window/google.maps)
+  (gmaps.Map. (sel1 :#div-map-canvas) (map-config-obj)))
 
 (set! (.-onload js/window) init)
+
